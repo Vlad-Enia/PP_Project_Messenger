a111e9fdd6d4f7fe70f0039eefa3ecbe07d39da1
Vlad-Enia <vladgio99@gmail.com>
2022-01-02 01:30:38 +0200

    Added GUI window for registering. Message logs are now stored in a directory corresponding to each user.

--- Client.py
+++ Client.py
@@ -2,6 +2,8 @@ import socket
 import pickle
 import rsa
 from threading import Thread
+import PySimpleGUI as sg
+import os
 
 SERVER_ADDRESS = '127.0.0.1'
 SERVER_PORT = 6001
@@ -14,7 +16,7 @@ def rec_msg(sender_socket):
         sender = msg['sender']
         encrypted_message = msg['body']
         decrypted_message = rsa.decrypt(encrypted_message, private_key).decode('utf8')
-        log_message(f'{client_id}_received_messages.txt', sender, decrypted_message)
+        log_message(f'{dir_path}/{client_id}_received_messages.txt', sender, decrypted_message)
         print(f'{sender}: {decrypted_message}')
 
 
@@ -33,18 +35,36 @@ def connect_and_register(server_address, server_port):
     sock.connect((server_address, server_port))
     print('Connected to Server')
     cl_id = ''
+
+    layout = [
+        [
+            sg.Text("Python Messaging App")
+        ],
+        [
+            sg.Text("Hello! Please register using an unique ID.", key='-SERVER_MESSAGE-')
+        ],
+        [
+            sg.In(size=(25, 1), enable_events=True, key="-ID-"),
+            sg.Button('Register', key='-BUTTON-', bind_return_key=True)
+        ]
+    ]
+    window = sg.Window("Register Window", layout, element_justification='center', element_padding=10)
+
     message = pickle.loads(sock.recv(4096))
     while message['type'] == 'register':
-        print('Server:', message['body'])
-        cl_id = input('ID: ')
-        send_msg('server', 'id', cl_id, cl_id, sock)
-        message = pickle.loads(sock.recv(4096))
+        event, values = window.read()
+        if event == sg.WIN_CLOSED:
+            break
+        elif event == '-BUTTON-' or event == 'Submit':
+            cl_id = values["-ID-"]
+            send_msg('server', 'id', cl_id, cl_id, sock)
+            message = pickle.loads(sock.recv(4096))
+            window['-SERVER_MESSAGE-'].update(message['body'])
+    window.close()
     print('Server:', message['body'])
+
     formatted_public_key = public_key.save_pkcs1(format='DER')
-    # sock.send(formatted_public_key)
     send_msg('server', 'pbkey', cl_id, formatted_public_key, sock)
-    # message = pickle.loads(sock.recv(4096))
-    # print('Server:', message['body'])
     return sock, cl_id
 
 
@@ -55,6 +75,10 @@ def log_message(filename, sender, message):
 
 
 server_socket, client_id = connect_and_register(SERVER_ADDRESS, SERVER_PORT)
+
+dir_path = f'./{client_id}_messages'
+
+os.mkdir(dir_path)
 
 print("Waiting for the other client to connect...")
 msg = pickle.loads(server_socket.recv(4096))
@@ -71,7 +95,7 @@ t.start()
 
 while True:
     msg = input()
-    log_message('message_history_log.txt',client_id, msg)
-    log_message(f'{client_id}_sent_messages.txt', client_id, msg)
+    log_message(f'{dir_path}/message_history_log.txt',client_id, msg)
+    log_message(f'{dir_path}/{client_id}_sent_messages.txt', client_id, msg)
     msg_encrypted = rsa.encrypt(msg.encode('utf8'), recipient_public_key)
     send_msg(recipient_id, 'msg', client_id, msg_encrypted, server_socket)
