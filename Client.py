bd7c8bedd14a01f9882d25d746c9856f5e05cf3c
Vlad-Enia <vladgio99@gmail.com>
2022-01-03 02:15:00 +0200

    Register window won't close while waiting for the other client to connect.

--- Client.py
+++ Client.py
@@ -12,6 +12,20 @@ SERVER_PORT = 6001
 public_key, private_key = rsa.newkeys(512)
 print(f'Your public key: {public_key}\n')
 
+def create_client_dir(client_id):
+    dir_path = f'./{client_id}_messages'
+    if os.path.isdir(dir_path):
+        shutil.rmtree(dir_path)
+    if client_id != '':
+        os.mkdir(dir_path)
+    return dir_path
+
+
+def log_message(filename, sender, message):
+    f = open(filename, 'a')
+    f.write(f'{sender}: {message}\n')
+    f.close()
+
 
 def rec_msg(sender_socket, msg_list):
     while True:
@@ -55,7 +69,7 @@ def connect_and_register(server_address, server_port):
     window = sg.Window("Register Window", layout, element_justification='center', element_padding=10)
 
     message = pickle.loads(sock.recv(4096))
-    while message['type'] == 'register':
+    while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED:
             window.close()
@@ -65,40 +79,35 @@ def connect_and_register(server_address, server_port):
             send_msg('server', 'id', cl_id, cl_id, sock)
             message = pickle.loads(sock.recv(4096))
             window['-SERVER_MESSAGE-'].update(message['body'])
+            window.refresh()
+            if message['type'] == 'register_ok':
+                formatted_public_key = public_key.save_pkcs1(format='DER')
+                send_msg('server', 'pbkey', cl_id, formatted_public_key, sock)
+                msg = pickle.loads(sock.recv(4096))
+                if msg['type'] == 'recipient_id':
+                    recipient_id = msg['body']
+                    recipient_formatted_public_key = sock.recv(4096)
+                    recipient_public_key = rsa.key.PublicKey.load_pkcs1(recipient_formatted_public_key, format='DER')
+                    print(f'{recipient_id} connected! Public key: {recipient_public_key}\n')
+                    break
     window.close()
-    print('Server:', message['body'])
-
-    formatted_public_key = public_key.save_pkcs1(format='DER')
-    send_msg('server', 'pbkey', cl_id, formatted_public_key, sock)
-    return sock, cl_id
+    return sock, cl_id, recipient_id, recipient_public_key
 
 
-def log_message(filename, sender, message):
-    f = open(filename, 'a')
-    f.write(f'{sender}: {message}\n')
-    f.close()
+server_socket, client_id, recipient_id, recipient_public_key = connect_and_register(SERVER_ADDRESS, SERVER_PORT)
+dir_path = create_client_dir(client_id)
 
-
-server_socket, client_id = connect_and_register(SERVER_ADDRESS, SERVER_PORT)
-
-dir_path = f'./{client_id}_messages'
-
-if os.path.isdir(dir_path):
-    shutil.rmtree(dir_path)
-os.mkdir(dir_path)
-
-print("Waiting for the other client to connect...")
-msg = pickle.loads(server_socket.recv(4096))
-if msg['type'] == 'recipient_id':
-    recipient_id = msg['body']
-    formatted_public_key = server_socket.recv(4096)
-    recipient_public_key = rsa.key.PublicKey.load_pkcs1(formatted_public_key, format='DER')
-    print(f'{recipient_id} connected! Public key: {recipient_public_key}\n')
+# print("Waiting for the other client to connect...")
+# msg = pickle.loads(server_socket.recv(4096))
+# if msg['type'] == 'recipient_id':
+#     recipient_id = msg['body']
+#     formatted_public_key = server_socket.recv(4096)
+#     recipient_public_key = rsa.key.PublicKey.load_pkcs1(formatted_public_key, format='DER')
+#     print(f'{recipient_id} connected! Public key: {recipient_public_key}\n')
 
 
 recipient_column = []
 sender_column = []
-
 layout = [
     [
         sg.Column(recipient_column, key='-RECIPIENT-'),
@@ -110,11 +119,11 @@ layout = [
         sg.Button('Send', enable_events=True, key='-SEND_BUTTON-', bind_return_key=True)
     ]
 ]
-
 window = sg.Window(f'{client_id} chat window', layout, element_justification='center')
 
 received_msg_list = []
 received_msg_list_copy = []
+
 t = Thread(target=rec_msg, args=(server_socket, received_msg_list))
 t.daemon = True
 t.start()
@@ -131,7 +140,6 @@ while True:
     elif event == '-SEND_BUTTON-':
         msg = values['-SEND_TEXT-']
         window['-SEND_TEXT-']('')      #clear input box when sending
-        # window.Refresh()
         window.extend_layout(window['-SENDER-'], [[sg.Text(msg)]])
         window.extend_layout(window['-RECIPIENT-'], [[sg.Text('')]])
         log_message(f'{dir_path}/message_history_log.txt', client_id, msg)
@@ -140,4 +148,5 @@ while True:
         msg_encrypted = rsa.encrypt(msg.encode('utf8'), recipient_public_key)
         send_msg(recipient_id, 'msg', client_id, msg_encrypted, server_socket)
 
+server_socket.close()
 window.close()
